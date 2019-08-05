1163b4d87a0b865c1d307a854731feae16583d9a
Cüneyt Aşkın <cuneyt.askin9@gmail.com>
2019-08-05 18:03:37 +0300

    Updates

---
+++ precache-manifest.f31a2b2d78bcf4b55ff613ad5522b60d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8662e32d1ac601c02585732a74e40a1",
+    "url": "/crm-demo/index.html"
+  },
+  {
+    "revision": "9b0be145a255d3223eea",
+    "url": "/crm-demo/static/css/main.176ecf78.chunk.css"
+  },
+  {
+    "revision": "e1e09e5f802630b7763d",
+    "url": "/crm-demo/static/js/2.e4359479.chunk.js"
+  },
+  {
+    "revision": "9b0be145a255d3223eea",
+    "url": "/crm-demo/static/js/main.e3d3f13f.chunk.js"
+  },
+  {
+    "revision": "595288a557ac7db7af3f",
+    "url": "/crm-demo/static/js/runtime~main.391320c9.js"
+  }
+]);
